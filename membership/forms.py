1de4f4ef82d2e0ede8c5c263432e03523a30c43e
djan-guy <erensaint8@gmail.com>
2026-01-22 11:44:40 +0000

    Delete membership directory

--- membership/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Membership
-
-class MembershipForm(forms.ModelForm):
-    class Meta:
-        model = Membership
-        fields = ['name', 'email', 'duration', 'pricing', 'payment', 'membership']
--- membership/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'membership'
-
-urlpatterns = [
-    path('', views.membership_page, name='membership_page'),
-    path('verification/', views.membership_verification, name='membership_verification'),
-]
