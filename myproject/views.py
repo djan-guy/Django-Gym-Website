fe4f7278828d2f04ee54e830bdff1e7f66ada462
djan-guy <erensaint8@gmail.com>
2026-01-22 11:44:47 +0000

    Delete myproject directory

--- myproject/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'home.html')
-
-def membership(request):
-    return render(request, 'membership.html')
