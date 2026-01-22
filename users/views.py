27694103c7172dade5b73cf1f91c7d0023645db3
djan-guy <erensaint8@gmail.com>
2026-01-22 11:48:19 +0000

    Add files via upload

---
+++ users/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from .forms import CustomUserCreationForm
+
+# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            login(request, form.save())
+            return redirect('pages:list')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'users/register.html', { 'form' : form })
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect('pages:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', { 'form' : form })
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('pages:list')
+    
+def profile_page(request):
+    return render(request, 'users/profile.html')
