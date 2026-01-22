61878be0e2195d1b456d50bbe727f88ef0b852fa
djan-guy <erensaint8@gmail.com>
2026-01-22 11:45:18 +0000

    Delete users directory

--- users/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from .models import MyUser
-
-class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model = MyUser
-        fields = ['username', 'password1', 'password2', 'email', 'dateOfBirth', 'address']
-    dateOfBirth = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
